add3cbe876056ec108f5e123f23085ca878a0b15
DoutorWho <130578013+DoutorWho@users.noreply.github.com>
2024-05-10 09:28:36 -0300

    Add files via upload

--- Projeto Eld/arquivos/banco.py
+++ Projeto Eld/arquivos/banco.py
@@ -1,13 +1,24 @@
-from os import stat
 import sqlite3
 if __name__ == '__main__':
     conexao = sqlite3.connect("bancodedados.db")
     cursor = conexao.cursor()
 
+
 class Pessoas:    
     @staticmethod
+    def perfil(email):
+        conexao = sqlite3.connect("bancodedados.db")
+        cursor = conexao.cursor()
+        cursor.execute("""
+            SELECT * FROM Pessoas
+            where email = ?""",(email,))    
+        perfil = cursor.fetchone()
+        arquivo =  'imagens_usarios/' + perfil[4]
+        sexo = 'Masculino' if perfil[3] == 'M' else 'Feminino'
+        pasta = [perfil[1], perfil[2], sexo, arquivo, str(perfil[5]).title(), perfil[6], str(perfil[8]).replace('.', ',')] # arquivo que vai ser retornado!
+        return pasta
+    @staticmethod
     def logar(email, senha):
-        print("accesei aqui")
         conexao = sqlite3.connect("bancodedados.db")
         cursor = conexao.cursor()
         cursor.execute("""
@@ -15,7 +26,6 @@ class Pessoas:
         where email = ?;""",(email,))
         nome = cursor.fetchall() 
         conexao.close()
-        print(nome)
         if nome == []:
             return [False, 'usuário não existe'] # 1 de usuário não existe
 
@@ -25,7 +35,21 @@ class Pessoas:
 
         return [False, 'senha inválida'] # 2 de senha inválida!
     @staticmethod
-    def adicionar_usuario(nome, email, sexo, senha):
+    def verificar_id_foto(email=None):
+        conexao = sqlite3.connect("bancodedados.db")
+        cursor = conexao.cursor()
+        if email == None:
+            cursor.execute("SELECT count(idPessoas) from Pessoas")
+            id_da_pessoa = str(int(cursor.fetchone()[0]) + 1)
+        else:
+            cursor.execute(""" 
+                SELECT idPessoas from Pessoas
+                where email = ?""",(email,))
+            id_da_pessoa = str(cursor.fetchone()[0])
+        conexao.close()
+        return id_da_pessoa
+    @staticmethod
+    def adicionar_usuario(nome, email, sexo, foto_de_perfil, forma_de_pagamento, preferencia, senha):
         nome = str(nome).title() # para deixar bonito!
         if sexo == 'Masculino':
             sexo = 'M'
@@ -33,30 +57,91 @@ class Pessoas:
             sexo = 'F'
         conexao = sqlite3.connect("bancodedados.db")
         cursor = conexao.cursor()
-        print("cadastrando 1")
         cursor.execute("""
             SELECT * FROM Pessoas
             where email = ?""", (email,))
         email_cadastrado = cursor.fetchall()
-        if email_cadastrado == []: # Se isso for igual, é pq não existe email cadastado!
+        if email_cadastrado == []: # Se isso for igual, é pq não existe esse email cadastado!
             cursor.execute("""
-                           INSERT INTO Pessoas(nome, email, sexo, senha, dinheiro) Values
-                           (?, ?, ?, ?, 2000)""",(nome, email, sexo, senha))
+                           INSERT INTO Pessoas(nome, email, sexo, foto_de_perfil, forma_de_pagamento, preferencia, senha, dinheiro) Values
+                           (?, ?, ?, ?, ?, ?, ?, 2000)""",(nome, email, sexo, foto_de_perfil, forma_de_pagamento, preferencia, senha))
             conexao.commit()
             conexao.close()
             return True
         return False
-
+    @staticmethod
+    def alterar_ususario(email, nome, sexo, forma_de_pagamento, preferencia, foto, senha):
+        from os import remove, path, getcwd
+        conexao = sqlite3.connect("bancodedados.db")
+        cursor = conexao.cursor()
+        if sexo == 'Masculino':
+            sexo = 'M'
+        else:
+            sexo = 'F'
+        # coloca pra excluir a foto aqui! No caso pegando o email e exlcuindo a foto antiga!
+        cursor.execute(""" 
+            SELECT foto_de_perfil FROM Pessoas
+            where email = ?""",(email,))
+        foto_excluir = cursor.fetchone()[0]
+        try:
+            remove(path.join(getcwd() + '\\arquivos' + '\\front_end' + '\\imagens_usarios' + f'\\{foto_excluir}')) # Para remover o arquivo!
+        except FileNotFoundError:
+            print("Arquivo não encontrado!")
+        # Colocando a foto no caminho certo, verificação de id
+        id_da_pessoa = Pessoas.verificar_id_foto(email) + '_' + foto
+        # Alterando os dados, a partir do email, já que é único!
+        cursor.execute(""" 
+            Update Pessoas
+                set 
+                nome = ?,
+                sexo = ?,
+                forma_de_pagamento = ?,
+                preferencia = ?,
+                foto_de_perfil = ?,
+                senha = ?
+            where email = ?""",(nome, sexo, forma_de_pagamento, preferencia, id_da_pessoa, senha, email))
+        conexao.commit()
+        conexao.close()
+        return id_da_pessoa
+    @staticmethod
+    def excluir_ususario(email):
+        from os import remove, path, getcwd
+        conexao = sqlite3.connect("bancodedados.db")
+        cursor = conexao.cursor()
+        # exclui a foto de perfil
+        cursor.execute(""" 
+            SELECT foto_de_perfil FROM Pessoas
+            where email = ?""",(email,))
+        foto_excluir = cursor.fetchone()[0]
+        try:
+            remove(path.join(getcwd() + '\\arquivos' + '\\front_end' + '\\imagens_usarios' + f'\\{foto_excluir}')) # Para remover o arquivo!
+        except FileNotFoundError:
+            print("Arquivo não encontrado!")
+        # excluir a conta
+        cursor.execute(""" 
+            DELETE FROM Pessoas
+            where email = ?""",(email,))
+        conexao.commit()
+        conexao.close()
 
 class Produtos:
     @staticmethod
-    def adicionar_produto(nome, descricao, imagem, preco):
+    def adicionar_produto(tipo, nome, descricao, imagem, preco):
+        """ 
+        Função que adiciona valores ao site!
+        Utilização:
+        tipo: Existem somente três tipos!
+        nome: Escolha o nome do produto
+        descricao: Escolha a descrição do produto!
+        imagem: Escolha uma imagem que esteja em img/compras! Mande somente o nome dela!
+        preco: Digite o preco! O preco tem que ser com '', ou seja, uma string!
+        """
         conexao = sqlite3.connect("bancodedados.db")
         cursor = conexao.cursor()
         cursor.execute("""
-            INSERT INTO Produtos(nome, descricao, imagem, preco) Values
-            (?, ?, ?, ?);
-            """,(nome, descricao, imagem, preco))
+            INSERT INTO Produtos(tipo, nome, descricao, imagem, preco) Values
+            (?, ?, ?, ?, ?);
+            """,(tipo, nome, descricao, imagem, preco))
         conexao.commit()
         conexao.close()
         print(f"O produto {nome} foi adicionado com sucesso!")
@@ -69,7 +154,7 @@ class Produtos:
         conexao.close()
         return produtos
     @staticmethod
-    def add_carrinho(email, idProduto, preco, quantidade):
+    def add_carrinho(email, idProduto, quantidade):
         conexao = sqlite3.connect("bancodedados.db")
         cursor = conexao.cursor()
         cursor.execute(""" 
@@ -77,9 +162,9 @@ class Produtos:
             where email = ?""",(email,))
         idUsuario = cursor.fetchall()[0][0]
         cursor.execute(""" 
-            INSERT INTO Carrinho(idPessoas, produto, preco, quantidade) Values
-            (?, ?, ?, ?)""",(idUsuario, idProduto, preco, quantidade))
-        conexao.commit()
+            INSERT INTO Carrinho(idPessoas, produto, quantidade) Values
+            (?, ?, ?)""",(idUsuario, idProduto, quantidade))
+        conexao.commit() # o PREÇO DO PRODUTO EU SEI A PARTIR DO PRODUTO!
         conexao.close()
     @staticmethod
     def ver_carrinho(email):
@@ -92,18 +177,61 @@ class Produtos:
         cursor.execute("""
             SELECT * FROM Carrinho
             WHERE idPessoas = ?""",(idPessoas,))
-        carrinho = cursor.fetchall()
+        produtos_no_carrinho = cursor.fetchall() # são os itens que estão no carrinho! O item 3 é o id
         cursor.execute("SELECT * FROM Produtos")
-        produtos = cursor.fetchall()
-        produtos_carrinho = []
-        for produto in produtos: # onde estão os produtos
-            produtos_carrinho.append([produto[1], produto[2], produto[3]])
-        print("Produtos do carrinho: ",produtos_carrinho)
-        for posicao, produto in enumerate(carrinho): # adicionar ao final os preco e a quantidade
-            produtos_carrinho[posicao].append([produto[3], produto[4]])
-    
-
-        return produtos_carrinho
+        produtos_disponiveis = cursor.fetchall() # são todos os produtos disponíveis
+        carrinho_usuario = []
+        for carrinho in produtos_no_carrinho:
+            for produtos in produtos_disponiveis:
+                if carrinho[2] == produtos[0]: # Aqui é o seguinte, o id do produto no carrinho é igual ao id do produto normal! Se for ele
+                    # se for ele executa isso, para pegar o produto!
+                    preco = produtos[5]
+                    quantidade = int(carrinho[3])
+                    itens = [carrinho[0], produtos[1], produtos[2], produtos[3], produtos[4], preco, quantidade] 
+                    carrinho_usuario.append(itens.copy())
+                    itens.clear()
+                    break
+        return carrinho_usuario
+    @staticmethod
+    def comprar_excluir(status, idCarrinho, email=None, preco=None): 
+        conexao = sqlite3.connect("bancodedados.db")
+        cursor = conexao.cursor()
+        if status == 'comprar':
+            cursor.execute("""
+                SELECT idPessoas, dinheiro FROM Pessoas
+                where email = ?""",(email,))
+            dados = cursor.fetchone()
+            idPessoas = dados[0]
+            dinheiro = dados[1]
+            if preco > dinheiro:
+                return False
+            cursor.execute("""
+                Update Pessoas
+                set dinheiro = dinheiro - ?
+                where idPessoas = ?""",(preco, idPessoas))
+            conexao.commit()
+            cursor.execute("""
+                DELETE FROM Carrinho
+                where idCompra = ?""",(idCarrinho,))
+            conexao.commit()
+            conexao.close()
+            return True
+        if status == 'excluir':
+            cursor.execute("""
+                DELETE FROM Carrinho
+                where idCompra = ?""",(idCarrinho,))
+            conexao.commit()
+        conexao.close()
+    @staticmethod
+    def cookie(email):
+        conexao = sqlite3.connect("bancodedados.db")
+        cursor = conexao.cursor()
+        cursor.execute(""" 
+            SELECT preferencia FROM Pessoas
+            where email = ?""",(email,))
+        preferencia = cursor.fetchone()[0]
+        conexao.close()
+        return preferencia
 '''conexao = sqlite3.connect("bancodedados.db")
 cursor = conexao.cursor()
 cursor.execute("""DROP TABLE Pessoas""")
@@ -116,6 +244,9 @@ cursor.execute("""
         nome TEXT NOT NULL,
         email TEXT NOT NULL,
         sexo TEXT NOT NULL CHECK(sexo IN ('M', 'F')),
+        foto_de_perfil TEXT NOT NULL,
+        forma_de_pagamento TEXT NOT NULL,
+        preferencia TEXT NOT NULL,
         senha TEXT NOT NULL,
         dinheiro FLAOT NOT NULL
         );
@@ -124,31 +255,31 @@ cursor.execute("""
     CREATE TABLE Carrinho(
         idCompra INTEGER PRIMARY KEY NOT NULL,
         idPessoas INTEGER NOT NULL,
-        produto TEXT NULL,
-        preco FLAOT NOT NULL,
+        produto INTEGER NULL,
         quantidade FLOAT NOT NULL
         );
     """)
 cursor.execute(""" 
     CREATE TABLE Produtos(
         idProdutos INTEGER PRIMARY KEY NOT NULL,
+        tipo TEXT NOT NULL,
         nome TEXT NOT NULL,
         descricao TEXT NOT NULL,
         imagem TEXT NOT NULL,
         preco TEXT NOT NULL
         );""")
-'''
-'''Produtos.adicionar_produto('Macarron', descricao='Macarrão bonito de comer', imagem="macarron.jpg", preco='7,20')
-Produtos.adicionar_produto('Hambúrgueres Doces', descricao='Hambúrgueres doces! Coma e viva!', imagem="docesham.jpg", preco='10,30')'''
+conexao.close()'''
+'''Produtos.adicionar_produto(tipo='doces_quadrados' ,nome='Macarron', descricao='Macarrão bonito de comer', imagem="macarron.jpg", preco='7,20')
+Produtos.adicionar_produto(tipo='doces_frescos', nome='Hambúrgueres Doces', descricao='Hambúrgueres doces! Coma e viva!', imagem="docesham.jpg", preco='10,30')
+Produtos.adicionar_produto(tipo='doces_epicos', nome="Bolo de Pote", descricao="Um bolo delecioso! Super racheado em um mini formato.", imagem="bolodepote.jpg", preco="14,00")'''
 
 '''conexao = sqlite3.connect("bancodedados.db")
 cursor = conexao.cursor()
 cursor.execute("SELECT * FROM Pessoas")
 print(cursor.fetchall())
 conexao.close()'''
+#conexao.close()
 
-#Produtos.adicionar_produto(nome="Bolo de Pote", descricao="Um bolo delecioso! Super racheado em um mini formato.", imagem="bolodepote.jpg", preco="14,00")
-#Produtos.adicionar_produto('Kit Macaron', "Compre e se divirta!", "lojadedoces.jpg", 10)
 
 # quando eu for comprar ele pega o id do usuário e nome do produto, e assim ele altera a quantidade, se caso o nome não existir, ele cria e coloca a quantidade desejada.
 
--- Projeto Eld/arquivos/main.py
+++ Projeto Eld/arquivos/main.py
@@ -1,25 +1,56 @@
-from time import sleep
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, session, sessions, url_for
+import os
+from flask import Flask, make_response, render_template, request, redirect, session, url_for
+from werkzeug.utils import secure_filename
 import banco
 app = Flask(__name__, template_folder='front_end', static_folder='front_end')
 app.secret_key = "legal"
-'''conexao = sqlite3.connect("lojadedoces.db")
-cursor = conexao.cursor()'''
-
+diretorio_imagens = os.path.join(os.getcwd() + '\\arquivos' + '\\front_end' + '\\imagens_usarios') 
 @app.route("/")
 def paginainicial():
     usuario = session.get('usuario', None)
     lista = banco.Produtos.lista_produtos()
-    return render_template("pagina_inicial.html", usuario=usuario, produtos=lista)
+    email = session.get("email", None)
+    preferencia = request.cookies.get("preferencia", None) 
+    if email != None:
+        resposta = banco.Produtos.cookie(email)
+        resp = make_response(render_template("pagina_inicial.html", usuario=usuario, produtos=lista, preferencia=resposta))
+        resp.set_cookie("preferencia", resposta)
+        return resp
+    return render_template("pagina_inicial.html", usuario=usuario, produtos=lista, preferencia=preferencia)
 
-@app.route("/cadastro")
+@app.route("/erro/<tipo_de_erro>")
+def erro(tipo_de_erro):
+    print("O tipo é: ", tipo_de_erro)
+    return render_template("erros.html", erro=tipo_de_erro)
+
+@app.route("/cadastro", methods=['POST', 'GET'])
 def cadastro():
-    mensagem_erro_cadastro = session.get('mensagem_cadastro')
-    mensagem_erro_login = session.get('mensagem_login')
-    session.pop("mensagem_cadastro", None)
-    session.pop("mensagem_login", None)
-    return render_template("cadastro.html", mensagem_erro_cadastro=mensagem_erro_cadastro, mensagem_erro_login=mensagem_erro_login)
+    if request.method == 'POST': # Aqui é quando ele entra no cadastro/login! Primeiro, ou seja, POST!
+        status = request.form['status']
+        return render_template("cadastro.html", status=status)
+    else: # se ele é do tipo GET. Ou seja, ou é a segunda vez por erro ou o usuário tá trolando!
+        email =  session.get("email")
+        mensagem_erro_cadastro = session.get('mensagem_cadastro')
+        mensagem_erro_login = session.get('mensagem_login')
+        session.pop("mensagem_cadastro", None)
+        session.pop("mensagem_login", None)
+        if email != None: # Aqui é se ele estiver trolando, isso já resolve!
+            return redirect(url_for("erro", tipo_de_erro="Você já está logado!"))
+        if (mensagem_erro_cadastro != None): # Aqui é se caso haja uma mensagem de erro!
+            return render_template("cadastro.html", mensagem_erro_cadastro=mensagem_erro_cadastro, status='cadastro')
+        if (mensagem_erro_login != None): #Aqui é se caso haja uma mensagem de erro!
+            return render_template("cadastro.html", mensagem_erro_login=mensagem_erro_login, status='login')
+        return redirect(url_for("paginainicial"))
+
+@app.route("/perfil")
+def perfil():
+    email = session.get("email")
+    if email != None:
+        resultados = banco.Pessoas.perfil(email) # entrar no banco de dados e pegar os dados!
+        status = session.get("status", None)
+        session.pop("status", None)
+        return render_template("perfil.html", status=status, usuario=resultados[0], email=resultados[1], sexo=resultados[2], imagem=resultados[3], forma=resultados[4], preferencia=resultados[5], dinheiro=resultados[6])
+    return redirect(url_for("erro", tipo_de_erro='Você precisa criar uma conta ou entrar na sua!'))
 
 @app.route("/carrinho")
 def carrinho():
@@ -29,25 +60,23 @@ def carrinho():
     # vendo os produtos do carrinho
     email = session.get('email')
     lista = banco.Produtos.ver_carrinho(email)
-    #
-    return render_template("carrinho.html", lista=lista)
-
-@app.route("/erro/<tipo_de_erro>")
-def erro(tipo_de_erro):
-    print("cheguei aqui")
-    print("O tipo é: ", tipo_de_erro)
-    return render_template("erros.html", erro=tipo_de_erro)
-
+    status = session.get("compras", None) # Aqui é se tá comprado ou excluido!
+    usuario = session.get("usuario", None)
+    session.pop("compras", None)
+    return render_template("carrinho.html", lista=lista, status=status, usuario=usuario)
 
 @app.route("/<pagina>/")
 def paginas(pagina):
     if pagina == 'pagina_inicial':
         return redirect(url_for("paginainicial"))
-    elif pagina == 'sobre':
-        return render_template("sobre.html")
-    elif pagina == "historiadaloja":
-        return render_template("sobre.html")
-    return render_template("erros.html",erro='página inválida')
+    elif (pagina == "historiadaloja") or (pagina == 'sobre'):
+        usuario = session.get("usuario", None)
+        return render_template("sobre.html", usuario=usuario)
+    elif pagina == 'perfil':
+        return redirect(url_for("perfil"))
+    elif pagina == "favicon.ico":
+        return "teste" # isso aqui é caso ele tente acessar uma rota já existente, como /cadastro, /carrinho
+    return redirect(url_for("erro",tipo_de_erro='página inválida'))
 
 # pegando as informações
 @app.route('/cadastro/submit_form', methods=['post'])
@@ -55,19 +84,31 @@ def submit():
     nome = request.form['nome']
     sexo = request.form['sexo']
     email = request.form['email']
+    forma_de_pagamento = request.form['forma_de_pagamento']
+    preferencia = request.form['preferencia']
+    foto_de_perfil = request.files['foto']
     senha = request.form['senha']
     confirmasenha = request.form['confirmasenha']
+    formato = foto_de_perfil.content_type.split("/")[1]
+    if formato not in ['jpeg', 'png']:
+        session['mensagem_cadastro'] = 'Erro, digite um arquivo jpeg ou png!'
+        return redirect(url_for("cadastro"))
     if senha != confirmasenha: # se a senha não for igual!
         session['mensagem_cadastro'] = 'Senha inválida! As senhas não são iguais'
         return redirect(url_for("cadastro"))
+    # Aqui é o id da Pessoa onde a foto será adicionada!
+    foto = banco.Pessoas.verificar_id_foto() + '_' +  str(foto_de_perfil.filename)  # Aqui é o seguinte, ele vai retornar o id do usuário e adiciona o nome da foto! Para a gente saber a qual id pessoa a foto!
+    SaveFoto = os.path.join(diretorio_imagens, secure_filename(foto)) #aqui vai ficar o diretório e o id da foto. Este "_" é para eu saber onde termina o id!
+    resposta = banco.Pessoas.adicionar_usuario(nome, email, sexo, foto, forma_de_pagamento, preferencia, senha) # aqui para adicionar a pessoa! E a foto!
+    foto_de_perfil.save(SaveFoto) # para salvar a foto
     # se caso a senha for verdadeira
-    resposta = banco.Pessoas.adicionar_usuario(nome, email, sexo, senha) # aqui para adicionar a pessoa!
     if resposta: # se for verdadeiro é pq o email ainda não foi cadastrado!
         session['usuario'] = nome # o nome do usuário para já cadastrar!
         session['email'] = email
+        session.pop("mensagem_cadastro", None) # aqui para apagar a mensagem de erro!
         return redirect(url_for("paginainicial"))
-    session.pop("mensagem_cadastro", None) # aqui para apagar a mensagem de erro!
-    return redirect(url_for("paginainicial"))
+    session['mensagem_cadastro'] = 'Esse email já existe!'
+    return redirect(url_for("cadastro"))
 
 @app.route("/login/submit_form", methods=['post'])
 def submita():
@@ -85,13 +126,72 @@ def submita():
 def add_carrinho():
     if request.method == 'POST':
         quantidade = int(request.form['quantidade'])
-        preco = float(request.form['preco'].replace(',','.'))
         produto = int(request.form['idProduto'])
         email = session.get('email')
-        banco.Produtos.add_carrinho(email=email, idProduto=produto, preco=preco, quantidade=quantidade)
+        banco.Produtos.add_carrinho(email=email, idProduto=produto, quantidade=quantidade)
         return redirect(url_for('carrinho'))
-        #return f"A quantidade: {quantidade} o preco {preco}, total {quantidade * preco}"
     return redirect(url_for('paginainicial'))
 
+@app.route("/carrinhoform/submit_form", methods=['post', 'GET'])
+def carrinho_compra_excluir():
+    if request.method == 'POST':
+        idCarrinho = request.form['idCarrinho']
+        comprar = request.form.get("comprar")
+        excluir = request.form.get("excluir")
+        if comprar != None:
+            preco = request.form['preco']
+            quantidade = request.form['quantidade']
+            email = session.get("email")
+            total = float(preco.replace(',','.')) * float(quantidade) # Aqui é o total que ele vai pagar!
+            retorno = banco.Produtos.comprar_excluir(status='comprar', idCarrinho=idCarrinho, email=email, preco=total)
+            if not retorno: # Ou seja, se ele for falso significa que a pessoa tá pobre!
+                session['compras'] = 'sem_dinheiro'
+                return redirect(url_for("carrinho"))
+            print("Passei aqui!")
+            session['compras'] = 'compra_sucesso'
+            return redirect(url_for("carrinho"))
+        if excluir != None:
+            banco.Produtos.comprar_excluir(status='excluir', idCarrinho=idCarrinho)
+            session['compras'] = 'excluido'
+            return redirect(url_for("carrinho"))
+    return redirect(url_for("erro", tipo_de_erro="Página não encontrada!"))
+
+@app.route("/alterar_perfil/submit_form", methods=['POST', 'GET'])
+def perfil_status():
+    tipo = request.form['tipo']
+    if request.method == 'POST':
+        if tipo == 'normal': # Isso quer dizer que ele quer alterar a conta!
+            status = request.form['status_conta'] #Aqui é para verificar o que fazer no inicio
+            session['status'] = status # Isso pra certificar lá na rota perfil! Pra ele passar!
+            if status == 'alterar':
+                return redirect(url_for("perfil"))
+            if status == 'excluir':
+                email = session['email']
+                banco.Pessoas.excluir_ususario(email)
+                session.clear()
+                return redirect(url_for("paginainicial"))
+            if status == 'sair':
+                session.clear()
+                return redirect(url_for("paginainicial"))
+        if tipo == 'novo_perfil':
+            nome = request.form['nome']
+            sexo = request.form['sexo']
+            forma_de_pagamento = request.form['forma_de_pagamento']
+            preferencia = request.form['preferencia']
+            foto = request.files['foto']
+            senha = request.form['senha']
+            email = session['email']
+            formato = foto.content_type.split("/")[1]
+            if formato not in ['jpeg', 'png']: 
+                session['status'] = 'Erro, digite um arquivo jpeg ou png!'
+                return redirect(url_for("perfil"))
+            nome_foto = str(foto.filename)
+            foto_do_perfil = banco.Pessoas.alterar_ususario(email=email, nome=nome, sexo=sexo, forma_de_pagamento=forma_de_pagamento, preferencia=preferencia, foto=nome_foto, senha=senha)
+            SaveFoto = os.path.join(diretorio_imagens, secure_filename(foto_do_perfil))
+            foto.save(SaveFoto)
+            session['status'] = 'alterar_sucesso'
+            session['usuario'] = nome
+        return redirect(url_for("perfil"))
+    return ''
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
